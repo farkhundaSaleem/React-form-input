7499a2df3dafe35879531cb7f574d923a01eb605
Farkhunda Saleem <farkhunda2108@gmail.com>
2023-10-03 23:43:25 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+
+
+import React, { useState } from "react";
+import './App.css'
+
+const App = () => {
+  const [newInput, setInput] = useState("");
+const [submittedInputs, setSubmittedInputs] = useState([]);
+
+   
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  setSubmittedInputs([...submittedInputs, newInput])
+    setInput("");
+  };
+
+  const onChangeHandler = (e) => {
+    setInput(e.target.value);
+ 
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmitHandler} >
+        <div>
+          <label htmlFor="typo">Enter : </label>
+          <input
+            type="text"
+            id="typo"
+            onChange={onChangeHandler}
+            value={newInput}
+          />
+        </div>
+
+        <div>
+          <button>Click</button>
+        </div>
+      </form>
+
+
+      <div>
+        <h2>Submitted Inputs:</h2>
+       <ul>
+        {submittedInputs.map((input, index)=>(
+          <li key={index}>{input}</li>
+        ))}
+       </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
